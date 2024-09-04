159ff504c7ccd1eaa0c959a9c6d7884420103366
vangasse <vangasse@ic.ufal.br>
2024-09-04 17:30:43 -0300

    :alembic: test: solo agent

--- ros_ws/src/crazyswarm/scripts/follow-nurbs.py
+++ ros_ws/src/crazyswarm/scripts/follow-nurbs.py
@@ -12,7 +12,7 @@ from online_path_planning_denurbs.scripts.robot_models import *
 from online_path_planning_denurbs.scripts.view_experiment import *
 
 
-INPUT_YAML_FILE_NAME = "online_path_planning_denurbs/config/config.yaml"
+INPUT_YAML_FILE_NAME = "online_path_planning_denurbs/config/config_1static_obs.yaml"
 yaml_utils = YAML_utils(filename=INPUT_YAML_FILE_NAME)
 
 TAKEOFF_DURATION    = 2
@@ -21,6 +21,8 @@ SAMPLING_TIME       = 0.1
 DEGREE              = 7
 NUM_SAMPLES         = 300
 KF_FIELD            = 2.0
+
+ALTITUDE = 1.
 
 s_delta = 0.05
 s_f = 0.3
@@ -51,8 +53,11 @@ if __name__ == "__main__":
     data = []
 
     # TAKEOFF
-    cf.takeoff(targetHeight=1.0, duration=TAKEOFF_DURATION)
+    cf.takeoff(targetHeight=ALTITUDE, duration=TAKEOFF_DURATION)
     timeHelper.sleep(TAKEOFF_DURATION)
+
+    previous_position = cf.position()
+    target_position = [1.5, 1.0]
 
     init_time = timeHelper.time()
     
@@ -64,13 +69,20 @@ if __name__ == "__main__":
         """ Setting for the curve """
         ctrl_points, weights = yaml_utils.getControlWeightsfromConfig(yaml_utils.read_yaml())
         knot = yaml_utils.read_yaml()["knotvector"]
-        # print(f'w = {weights}')
         evalpts_x, evalpts_y, curve = pathNURBS.nurbs(degree=DEGREE, points=ctrl_points, weigths=weights, dt=1/NUM_SAMPLES, knot=knot)
 
         """ Update robot using the vector field control """
         curve.delta = 1/15000
         eval_func = curve.evaluate_single
-        field, s_star = compute_field(eval_func, pos=cf.position()[0:2], s_i = s_i, s_f = s_f, Kf=KF_FIELD, vr=vrobot)
+
+        current_position = cf.position()
+
+        if np.linalg.norm(current_position[0:2] - target_position) < 0.1:
+            break
+        elif current_position[0] > 1.5 or current_position[1] > 1:
+            break
+
+        field, s_star = compute_field(eval_func, pos=current_position[0:2], s_i = s_i, s_f = s_f, Kf=KF_FIELD, vr=vrobot)
         curve.delta = 1/NUM_SAMPLES
 
         if s_star - s_delta <= 0:
@@ -88,7 +100,10 @@ if __name__ == "__main__":
         # Apply Command
         ###################
         """ send control (vel) to the robot """
-        v = np.array([field[0], field[1], 0])
+        kp = 0.1
+        altitude_control = kp*(ALTITUDE - current_position[2])
+
+        v = np.array([field[0], field[1], altitude_control])
         cf.cmdVelocityWorld(v, yawRate=0)
         ###################
         
@@ -96,9 +111,21 @@ if __name__ == "__main__":
         spent_time = loop_end - loop_start
         timeHelper.sleep(SAMPLING_TIME - spent_time)
 
+        ###################
+        # RECORD DATA
+        ###################
 
-    df = pd.DataFrame(data, columns=['x', 'y', 'z', 'vx', 'vy', 'vz', 't', 'id'])
+        data.append([current_position[0], current_position[1],
+                     (current_position[0] - previous_position[0])/SAMPLING_TIME, (current_position[1] - previous_position[1])/SAMPLING_TIME,
+                     v[0], v[1],
+                     loop_end - init_time, 1
+        ])
+        previous_position = current_position
+
+        ###################
+
+
+    df = pd.DataFrame(data, columns=['x', 'y', 'vx', 'vy', 'ux', 'uy', 't', 'id'])
     df.to_csv("experiment.csv", index=False)
 
-    cf.land(targetHeight=0.04, duration=2.5)
-    timeHelper.sleep(TAKEOFF_DURATION)
+    cf.cmdVelocityWorld([0., 0., 0.], yawRate=0)
